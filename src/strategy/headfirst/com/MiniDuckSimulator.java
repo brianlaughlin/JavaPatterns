1f7f43909f90b08acb9353eef0368b87fac377f5
Brian Laughlin <brian@MSI>
2019-04-10 20:00:54 -0700

    Added Class name to Strategy Pattern for ducks

--- src/strategy/headfirst/com/MiniDuckSimulator.java
+++ src/strategy/headfirst/com/MiniDuckSimulator.java
@@ -13,11 +13,13 @@ public class MiniDuckSimulator {
     
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
+        System.out.println("I am " + mallard.getClass().getSimpleName());
         mallard.performFly();
         mallard.performQuack();
         
         
         Duck model = new ModelDuck();
+        System.out.println("I'm a " + model.getClass().getSimpleName());
         model.performFly();
         model.setFlyBehavior(new FlyRocketPowered());
         model.performFly();
