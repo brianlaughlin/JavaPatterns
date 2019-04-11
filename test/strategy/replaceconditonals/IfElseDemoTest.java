c9b22d73fa9dc26e1987f006b1e1128dc334c5fb
Brian Laughlin <brian@MSI>
2019-04-10 20:28:06 -0700

    Added Test class

---
+++ test/strategy/replaceconditonals/IfElseDemoTest.java
@@ -0,0 +1,29 @@
+package strategy.replaceconditonals;
+
+import static org.junit.Assert.*;
+
+public class IfElseDemoTest {
+    @Test
+    public void low() {
+        assertEquals(1825, insuranceFor(5000), 0.01);
+    }
+
+    @Test
+    public void medium() {
+        assertEquals(38600, insuranceFor(25000), 0.01);
+    }
+
+    @Test
+    public void high() {
+        assertEquals(78500, insuranceFor(50000), 0.01);
+    }
+
+    @Test
+    public void veryHigh() {
+        assertEquals(106400, insuranceFor(100_000), 0.01);
+    }
+
+    private double insuranceFor(double income) {
+        return new IfElseDemo().calculateInsurance(income);
+    }
+}
